5307e972d77fca12eace48f4f3968861ab885acb
ahmad-tb04 <ahmad81.tbakhi@gmail.com>
2026-01-27 21:37:43 +0300

    first-commit

---
+++ E-Com/E-Com.Core/Entities/Product/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Com.Core.Entities.Product
+{
+    public class Category : BaseEntity<int>
+    {
+        public string? Description { get; set; }
+    }
+}
